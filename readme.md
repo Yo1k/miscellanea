13b0de597be896e8d7e9bd1c0f2509b8ef54bb2e
Stanislav Kovalenko <stanislav.l.kovalenko@gmail.com>
2022-07-08 15:37:11 +0300

    Improve 'appearance()' and add `union_intervals()`

--- readme.md
+++ readme.md
@@ -36,13 +36,3 @@ alphabet and the total number of animal names beginning with that letter) in pro
 Use function `appearance(intervals)` to find total intersection time of three lists of 
 intervals: 'lesson', 'tutor', 'pupil'.
 
-**Remark: the description of Task_3 has a mistake**. The description says "Under even indexes 
-(starting from 0) is the time of entry to the lesson, under odd - the time of exit from the lesson."
-That means that all lists with timestamps are ordered by ascending. However, there is mistake in 
-the 2nd set of data in the `tests` list. `'pupil'` list is not ordered:
-element `1_594_704_500` goes before `1_594_702_807`. Note that even after sorting 'pupil' list 
-'answer' is wrong in the test case.
-
-To exclude data that violates the conditions of the task, asserts have been added in the code to 
-check the  ascending order of elements in lists, which can slow down the function on large data.
-The test case with the error is present in the code, but commented out.
--- yo1k/misc/miscellanea.py
+++ yo1k/misc/miscellanea.py
@@ -243,7 +243,7 @@ def appearance(intervals: dict[str, list[int]]) -> int:
     'tutor', 'pupil'. Each list consists of even number of timestamps elements in seconds:
     for 'lesson' they are start and end of the lesson;
     for 'tutor', 'pupil' they are timestamps of entrance and exit to the lesson. That is all
-    lists are ordered by ascending.
+    lists are ordered by ascending of elements with even (odd) indexes.
     """
     lesson: list[int] = intervals["lesson"]
     tutor: list[int] = intervals["tutor"]
@@ -252,13 +252,12 @@ def appearance(intervals: dict[str, list[int]]) -> int:
     assert len(lesson) % 2 == 0, f"`lesson` has odd number of elements: {len(lesson)}"
     assert len(tutor) % 2 == 0, f"`tutor` has odd number of elements: {len(tutor)}"
     assert len(pupil) % 2 == 0, f"`pupil` has odd number of elements: {len(pupil)}"
-    assert lesson == sorted(lesson), f"`lesson` is unsorted: {lesson}"
-    assert tutor == sorted(tutor), f"`tutor` is unsorted: {tutor}"
-    assert pupil == sorted(pupil), f"`pupil` is unsorted: {pupil}"
 
     total_intersection: list[int] = (
-            intersected_intervals(intersected_intervals(lesson, tutor), pupil)
+            intersected_intervals(
+                    intersected_intervals(lesson, union_intervals(tutor)), union_intervals(pupil))
     )
+
     return sum_intervals(total_intersection)
 
 
@@ -293,6 +292,20 @@ def intersected_intervals(lst1: list[int], lst2: list[int]) -> list[int]:
     return result
 
 
+def union_intervals(lst: list[int]) -> list[int]:
+    """Creates new list of union of overlapping intervals from the origin list."""
+    result: list[int] = []
+    result.extend([lst[0], lst[1]])
+    i = 0
+    for j in range(1, len(lst) // 2):
+        if result[2 * i + 1] > lst[2 * j]:
+            result[2 * i + 1] = max(result[2 * i + 1], lst[2 * j + 1])
+        else:
+            i += 1
+            result.extend([lst[2 * j], lst[2 * j + 1]])
+    return result
+
+
 if __name__ == "__main__":
     # Task_1
     # 1st task part
@@ -319,22 +332,17 @@ if __name__ == "__main__":
                       'tutor': [1594663290, 1594663430, 1594663443, 1594666473]},
              'answer': 3117
              },
-
-            # This test case is wrong. 'pupil' list is not ordered:
-            # element 1_594_704_500 goes before 1_594_702_807.
-            # However, even after 'pupil' list sorting 'answer' is wrong in the test case.
-
-            # {'data': {'lesson': [1594702800, 1594706400],
-            #           'pupil': [1594702789, 1594704500, 1594702807, 1594704542, 1594704512,
-            #                     1594704513, 1594704564, 1594705150, 1594704581, 1594704582,
-            #                     1594704734, 1594705009, 1594705095, 1594705096, 1594705106,
-            #                     1594706480, 1594705158, 1594705773, 1594705849, 1594706480,
-            #                     1594706500, 1594706875, 1594706502, 1594706503, 1594706524,
-            #                     1594706524, 1594706579, 1594706641],
-            #           'tutor': [1594700035, 1594700364, 1594702749, 1594705148, 1594705149,
-            #                     1594706463]},
-            #  'answer': 3577
-            #  },
+            {'data': {'lesson': [1594702800, 1594706400],
+                      'pupil': [1594702789, 1594704500, 1594702807, 1594704542, 1594704512,
+                                1594704513, 1594704564, 1594705150, 1594704581, 1594704582,
+                                1594704734, 1594705009, 1594705095, 1594705096, 1594705106,
+                                1594706480, 1594705158, 1594705773, 1594705849, 1594706480,
+                                1594706500, 1594706875, 1594706502, 1594706503, 1594706524,
+                                1594706524, 1594706579, 1594706641],
+                      'tutor': [1594700035, 1594700364, 1594702749, 1594705148, 1594705149,
+                                1594706463]},
+             'answer': 3577
+             },
             {'data': {'lesson': [1594692000, 1594695600],
                       'pupil': [1594692033, 1594696347],
                       'tutor': [1594692017, 1594692066, 1594692068, 1594696341]},
